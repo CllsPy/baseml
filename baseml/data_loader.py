b531fdace529fe6f01c2d1e313510aebdf2d889e
Cllspy <ifal.edu.2017@gmail.com>
2025-12-17 11:52:57 -0300

    create load_data to load csv file

--- baseml/data_loader.py
+++ baseml/data_loader.py
@@ -1,6 +1,15 @@
+import pandas as pd
+
 def build_path(dataset_name):
     tmp = "data/"
     return tmp + f"{dataset_name}" + ".csv"
 
+def load_data(dataset_name):
+    base_url = build_path(dataset_name)
+    data_frame = pd.read_csv(base_url)
+    return data_frame
+
+    
 if __name__ == "__main__":
     print(build_path("sample"))
+    print(load_data("sample"))
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,18 @@
 from baseml.data_loader import build_path
 from pathlib import Path
 
+import pytest
+import pandas as pd
 
-class TestDataValidator:
-    def test_path_exists(self):
-        path = build_path("sample")
-        assert Path(path).exists()
+@pytest.fixture
+def base_url():
+    return "sample"
+
+def test_path_exists(base_url):
+    path = build_path(base_url)
+    assert Path(path).exists()
+
+def test_load_data(base_url):
+    path = build_path(base_url)
+    data_frame = pd.read_csv(path)
+    assert not data_frame.empty
