e17d2075d9250b819fbcb778323b03a700e69f5d
Cllspy <ifal.edu.2017@gmail.com>
2025-12-19 07:42:55 -0300

    build setup models (agregate models); setup_grid (combine params from yaml and models from setup models

--- baseml/train_models.py
+++ baseml/train_models.py
@@ -1,6 +1,14 @@
 from baseml.data_loader import load_data
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
+from sklearn import linear_model
+from sklearn import svm
+
+import yaml
+
+with open("config/model.yaml", "r") as f:
+    params_yaml = yaml.safe_load(f)
 
 def split_data(dataset_name: str, target: str):
     data_frame = load_data(dataset_name)
@@ -15,10 +23,34 @@ def split_data(dataset_name: str, target: str):
     return X_train, X_test, y_train, y_test
 
 
+def setup_models():
+    models = {
+        "LinearRegression":  linear_model.LinearRegression(),
+        "SVR": svm.SVR()
+
+    }
+
+    return models
+
+def setup_grid():
+    models = setup_models()
+    grid_search = []
+
+    for key, value in models.items():
+        model_parameters = params_yaml[key]
+        estimator = GridSearchCV(value, model_parameters)
+        grid_search.append(estimator)
+    
+    return grid_search
+   
+
+
+
 if __name__=='__main__':
-    X_train, X_test, y_train, y_test = (split_data("sample", "target"))
-    
-    print(X_train.shape, y_train.shape)
-    print(X_test.shape, y_test.shape)
-    print(X_test.shape[1], X_train.shape[1])
-    
+    X = [[0, 0], [2, 2], [2, 2], [2, 2], [2, 2]]
+    y = [0.5, 2.5, 2.5, 2.5, 2.5]
+
+    grid = setup_grid()
+    for x in grid:
+        x.fit(X, y)
+        print(x.predict([[1, 1]]))
